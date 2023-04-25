f634351ace4dfe352a1ac1129196723fd2e884ef
Yuvraj Rathva <rathva.1@iitj.ac.in>
2023-04-26 02:38:19 +0530

    homepage

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,14 +1,55 @@
-import React from 'react'
-import bg from "./assets/Gradient-bg.svg";
+import React from "react";
+import map from "./assets/map.svg";
+import road from "./assets/road.svg";
+import logo from "./assets/logo.png";
 import "./Homepage.css";
 
 const Homepage = () => {
   return (
-        <div className="herosection-maincontainer">
-        <div className="herosection-container"></div>
-            {/* <img src={bg} alt="" /> */}
+    <div className="herosection-maincontainer">
+      <div className="herosection-container">
+        <div>
+          <div className="logo">
+            <img src={logo} />
+          </div>
         </div>
-  )
-}
+        <div className="herosection-topContainer">
+          <div className="herosection-topLeft">
+            <div className="herosection-text">
+              <h2>
+                Predicting and preventing car accidents is critical for public
+                safety.
+              </h2>
+              <p>
+                Every year, countless lives are lost or affected by car
+                accidents. But with the power of data and analysis, we can turn
+                this around. Our website provides insights into real-time car
+                accident prediction, hotspot locations, casualty analysis, and
+                more. By styding the causes and effects of accidents, we can
+                help make our roads safer and prevent future tragendies.
+              </p>
+              <span>
+                "Accidents can be avoided if we take the time to understand the
+                root cause and take preventive measures." - John D. Rockefeller
+                Jr.
+              </span>
+            </div>
+          </div>
+          <div className="herosection-topRight">
+            <img src={map} alt="" />
+          </div>
+        </div>
+        <div className="herosection-bottomContainer">
+          <div className="herosection-bottomLeft">
+            <div className="button">Button</div>
+          </div>
+          <div className="herosection-bottomRight">
+            <img src={road} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
